ea8ab7d7e92dde2543b1ec88ec85789dcc4dee31
orhi <orhi@puppetmaster.(none)>
2018-05-03 22:50:00 +0300

    Clearing the last warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -18,7 +18,7 @@ public class Greeter {
   }
 
 /**
- * This is some comment for thio paragrapth 
+ * This is some comment for thio paragrapth.
  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
